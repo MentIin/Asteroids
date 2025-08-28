89e7f7326b8f49290499a5cf956e2c9530762eb1
MentIin <81743613+MentIin@users.noreply.github.com>
2025-08-28 13:45:23 +0300

    Start working on Unity ads service

---
+++ Assets/CodeBase/Interfaces/Infrastructure/Services/UnityAdsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+namespace CodeBase.Interfaces.Infrastructure.Services
+{
+    public class UnityAdsService : IAdsService, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+    {
+        public event Action RewardedVideoReady;
+        public bool IsRewardedVideoReady { get; }
+        public UniTask Initialize()
+        {
+            
+        }
+
+        public void ShowRewardedVideo(Action onVideoFinished)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
