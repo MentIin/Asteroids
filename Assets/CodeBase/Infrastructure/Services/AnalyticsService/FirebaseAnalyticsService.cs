9509a8d99b56842bcbe1583b9b342b80cefc6806
MentIin <81743613+MentIin@users.noreply.github.com>
2025-08-29 12:44:19 +0300

    fully implement FirebaseAnalyticsService

--- Assets/CodeBase/Infrastructure/Services/AnalyticsService/FirebaseAnalyticsService.cs
+++ Assets/CodeBase/Infrastructure/Services/AnalyticsService/FirebaseAnalyticsService.cs
@@ -1,14 +1,74 @@
 ﻿using CodeBase.Interfaces.Infrastructure.Services;
 using Cysharp.Threading.Tasks;
+using Firebase;
+using Firebase.Analytics;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace CodeBase.Infrastructure.Services.AnalyticsService
 {
     public class FirebaseAnalyticsService : IAnalyticsService
     {
-        public UniTask Initialize()
+        private FirebaseApp _app;
+        private bool _isInitialized;
+
+        public async UniTask Initialize()
         {
+            var dependencyStatus = await FirebaseApp.CheckAndFixDependenciesAsync();
             
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                _app = FirebaseApp.DefaultInstance;
+                _isInitialized = true;
+                Debug.Log("Firebase Analytics initialized successfully");
+            }
+            else
+            {
+                Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}");
+                _isInitialized = false;
+            }
+        }
+
+        public UniTask SendEvent(string eventName)
+        {
+            if (!_isInitialized) return UniTask.CompletedTask;
+            
+            FirebaseAnalytics.LogEvent(eventName);
             return UniTask.CompletedTask;
+        }
+
+        public UniTask SendEvent(string eventName, Dictionary<string, object> parameters)
+        {
+            if (!_isInitialized) return UniTask.CompletedTask;
+
+            var parameterArray = new Parameter[parameters.Count];
+            var index = 0;
+            
+            foreach (var param in parameters)
+            {
+                parameterArray[index] = ConvertToFirebaseParameter(param.Key, param.Value);
+                index++;
+            }
+            
+            FirebaseAnalytics.LogEvent(eventName, parameterArray);
+            return UniTask.CompletedTask;
+        }
+
+        private Parameter ConvertToFirebaseParameter(string key, object value)
+        {
+            switch (value)
+            {
+                case int intValue:
+                    return new Parameter(key, intValue);
+                case long longValue:
+                    return new Parameter(key, longValue);
+                case double doubleValue:
+                    return new Parameter(key, doubleValue);
+                case float floatValue:
+                    return new Parameter(key, floatValue);
+                default:
+                    return new Parameter(key, value.ToString());
+            }
         }
     }
 }
